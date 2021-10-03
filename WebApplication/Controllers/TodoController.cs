339c0b11e2383a8d0014f9ba82f79ca76f0aa62c
vancodocton <duongtruong.nguyenky@gmail.com>
2021-10-03 13:50:29 +0700

    Create controller and view Todo

---
+++ WebApplication/Controllers/TodoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace WebApplication.Controllers
+{
+    public class TodoController : Controller
+    {
+        // GET: Todo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
