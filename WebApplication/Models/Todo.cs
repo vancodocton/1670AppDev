7fd4ca9e3d04af9c4a468086de55258f0d1c04cc
vancodocton <duongtruong.nguyenky@gmail.com>
2021-10-03 13:37:31 +0700

    Add Todo model

---
+++ WebApplication/Models/Todo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WebApplication.Models
+{
+    public class Todo
+    {
+        public int ID { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public Todo()
+        {
+        }
+
+        public Todo(int id, string description, DateTime dueDate)
+        {
+            this.ID = id;
+            this.Description = description;
+            this.DueDate = dueDate;
+        }
+        public Todo(Todo todo) : this(todo.ID, todo.Description, todo.DueDate) { }
+    }
+}
