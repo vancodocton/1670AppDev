7338b6524809934f9b5ae2de980a1282940766ee
vancodocton <duongtruong.nguyenky@gmail.com>
2021-10-03 21:03:08 +0700

    Add Index for TodoController

--- WebApplication/Controllers/TodoController.cs
+++ WebApplication/Controllers/TodoController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication.Models;
 namespace WebApplication.Controllers
 {
     public class TodoController : Controller
     {
+        private ApplicationDbContext _context = new ApplicationDbContext();
         // GET: Todo
         public ActionResult Index()
         {
-            return View();
+            List<Todo> todos = _context.Todos.ToList();
+            return View(todos);
         }
     }
 }
